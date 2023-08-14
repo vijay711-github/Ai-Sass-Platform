3e1cb691902ae9039cda9fa90edc27baf95c72ad
vijay711-github <vj7112000@gmail.com>
2023-08-14 12:39:43 +0530

    added clerk library for user authentication and added sign-in-up routes and middleware

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const DashboardPage = () => {
-  return (
-    <div>Dashbaord page(Authorized)</div>
-  )
-}
+	return (
+		<div className="flex justify-around">
+			<div>Dashbaord page(Authorized)</div>
+			<div>
+				<UserButton afterSignOutUrl="/" />
+			</div>
+		</div>
+	);
+};
 
 export default DashboardPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Genius',
-  description: 'Ai Platform',
-}
+	title: "Genius",
+	description: "Ai Platform",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={inter.className}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
 }
