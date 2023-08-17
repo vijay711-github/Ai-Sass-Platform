bc9f306033494a6e212b5f9b979389683ef5f7e3
vijay711-github <vj7112000@gmail.com>
2023-08-18 00:41:12 +0530

    Completed Video Generation using Replicate Ai Api

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import Heading from "@/components/Heading";
+import { FileAudio, MessageSquare, Music, VideoIcon } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "./constant";
+import { ChatCompletionRequestMessage } from "openai";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { Empty } from "@/components/Empty";
+import { Loader } from "@/components/Loader";
+import { cn } from "@/lib/utils";
+import { UserAvatar } from "@/components/UserAvatar";
+import { BotAvatar } from "@/components/BotAvatar";
+
+const VideoPage = () => {
+	const router = useRouter();
+	const [video, setVideo] = useState<string>();
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			prompt: "",
+		},
+	});
+
+	const isLoading = form.formState.isSubmitting;
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		try {
+			setVideo(undefined);
+
+			const response = await axios.post("/api/video", values);
+			setVideo(response.data[0]);
+
+			form.reset();
+		} catch (error: any) {
+			console.log(error);
+		} finally {
+			router.refresh();
+		}
+	};
+
+	return (
+		<div>
+			<Heading
+				title="Video Generation"
+				description="Turn your prompt into video."
+				icon={VideoIcon}
+				iconColor="text-orange-700"
+				bgColor="bg-orange-700/10"
+			/>
+			<div className="px-4 lg:px-8">
+				<div>
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+						>
+							<FormField
+								name="prompt"
+								render={({ field }) => (
+									<FormItem className="col-span-12 lg:col-span-10">
+										<FormControl className="m-0 p-0">
+											<Input
+												className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+												disabled={isLoading}
+												placeholder="Tiger playing with his kids"
+												{...field}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<Button
+								className="col-span-12 lg:col-span-2 w-full"
+								type="submit"
+								disabled={isLoading}
+								size="icon"
+							>
+								Generate
+							</Button>
+						</form>
+					</Form>
+				</div>
+				<div className="space-y-4 mt-4">
+					{isLoading && (
+						<div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+							<Loader />
+						</div>
+					)}
+					{!video && !isLoading && <Empty label="No video generated." />}
+					{video && (
+						<video
+							controls
+							className="w-full aspect-video mt-8 rounded-lg border bg-black"
+						>
+							<source src={video} />
+						</video>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VideoPage;
